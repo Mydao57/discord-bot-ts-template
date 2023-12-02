a79756b7edbede135f3adf5eb3acbd916a643759
HugoPC <hugothiebaut22@gmail.com>
2023-12-02 19:46:12 +0100

    Build folder removed

--- build/base/enums/Category.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Category;
-(function (Category) {
-    Category["Utilities"] = "Utilities";
-    Category["Developer"] = "Developer";
-})(Category || (Category = {}));
-exports.default = Category;
